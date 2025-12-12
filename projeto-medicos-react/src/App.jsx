68f37dab5186e8c25993dff86ccb931a96bee8c4
Paulo2991 <paulovaz38034@gmail.com>
2025-12-12 20:30:21 -0300

    Ajuste No Projeto

--- projeto-medicos-react/src/App.jsx
+++ projeto-medicos-react/src/App.jsx
@@ -1,10 +1,19 @@
-import './App.css'
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import SejaVoluntario from './pages/sejaVoluntario/SejaVoluntario'
+import Home from './pages/Home/Home'
+import Header from './components/Header/Header'
+import "./global.scss"
 function App() {
 
   return (
     <>
-      <p>Olá</p>
+    <BrowserRouter>
+     <Header />
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="/voluntario" element={<SejaVoluntario />} />
+      </Routes>
+     </BrowserRouter>
     </>
   )
 }
--- projeto-medicos-react/src/components/Header/Header.jsx
+++ projeto-medicos-react/src/components/Header/Header.jsx
@@ -1,8 +1,17 @@
+import { Link } from "react-router-dom";
+import Logo from  "../../assets/Logo.png"
 function Header(){
     return (
         <>
            <header>
-               <h1>Eu sou a Header</h1>
+               <div>
+                    <img src={Logo} alt="Logo do site, coração verde" />
+                    <Link to="">Médicos & Dentistas</Link>
+               </div>
+               <nav>
+                 <Link>Home</Link>
+                 <Link>Seja Voluntário</Link>
+               </nav>
             </header> 
         </>
     )
