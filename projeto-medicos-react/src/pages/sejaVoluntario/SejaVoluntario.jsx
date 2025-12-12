73c3a2bf66da4ffb42949bbff4ecf246f31387c2
Paulo2991 <paulovaz38034@gmail.com>
2025-12-11 22:24:23 -0300

    Projeto-Medico-Inicio

---
+++ projeto-medicos-react/src/pages/sejaVoluntario/SejaVoluntario.jsx
@@ -0,0 +1,11 @@
+function SejaVoluntario(){
+    return(
+        <>
+            <main>
+                <h2>Seja Voluntário</h2>
+            </main>
+        </>
+    )
+}
+
+export default SejaVoluntario;
